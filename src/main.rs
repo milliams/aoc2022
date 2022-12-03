47548ea2d9314856a36f0d26876d5c87a2baeeb8
Matt Williams <matt@milliams.com>
2022-12-03 21:42:28 +0000

    Add day 3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 use anyhow::{Context, Result, bail};
+use itertools::Itertools;
 
 macro_rules! read_lines {
     ($expression:expr) => {
@@ -103,6 +105,53 @@ where
     Ok(round_scores.iter().sum())
 }
 
+fn calculate_backpack_score<I>(lines: I) -> Result<u32>
+where
+    I: IntoIterator,
+    I::Item: AsRef<str>,
+{
+    let mut item_priorities = vec![];
+    for line in lines {
+        let line = line.as_ref();
+        let compartment_size = line.len() / 2;
+        let left: HashSet<char> = HashSet::from_iter(line[..compartment_size].chars());
+        let right: HashSet<char> = HashSet::from_iter(line[compartment_size..].chars());
+        let overlap = left.intersection(&right).take(1).nth(0).context("No misplaced item")?;
+        let priority = match overlap {
+            'A'..='Z' => *overlap as u32 - 38,  // A-Z = 27-52
+            'a'..='z' => *overlap as u32 - 96,  // a-z = 1-26
+            _ => bail!("Invalid backpack item")
+        };
+        item_priorities.push(priority);
+    }
+    Ok(item_priorities.iter().sum())
+}
+
+fn identify_group_badge<I>(lines: I) -> Result<u32>
+where
+    I: IntoIterator,
+    I::Item: AsRef<str>,
+{
+    let mut group_priorities = vec![];
+    for chunk in &lines.into_iter().chunks(3) {
+        let mut group_backpacks = vec![];
+        for line in chunk {
+            let line = line.as_ref();
+            let backpack: HashSet<char> = HashSet::from_iter(line.chars());
+            group_backpacks.push(backpack);
+        }
+        let first_intersection = HashSet::from_iter(group_backpacks[0].intersection(&group_backpacks[1]).into_iter().map(|c| *c));
+        let total_intersection = first_intersection.intersection(&group_backpacks[2]).take(1).nth(0).context("No matching group badge")?;
+        let priority = match total_intersection {
+            'A'..='Z' => *total_intersection as u32 - 38,  // A-Z = 27-52
+            'a'..='z' => *total_intersection as u32 - 96,  // a-z = 1-26
+            _ => bail!("Invalid backpack item")
+        };
+        group_priorities.push(priority);
+    }
+    Ok(group_priorities.iter().sum())
+}
+
 fn day1() -> Result<u32> {
     get_max(read_lines!("day1.txt"), 3).context("Getting calorie inventory")
 }
@@ -111,9 +160,18 @@ fn day2() -> Result<u32> {
     calculate_rps_score(read_lines!("day2.txt")).context("Calculating RPS scores")
 }
 
+fn day3() -> Result<()> {
+    let a = calculate_backpack_score(read_lines!("day3.txt")).context("Calculating backpack score")?;
+    let b = identify_group_badge(read_lines!("day3.txt")).context("Calculating group badges")?;
+    println!("Day 3: {}", a);
+    println!("Day 3: {}", b);
+    Ok(())
+}
+
 fn main() -> Result<()> {
     println!("Day 1: {}", day1()?);
     println!("Day 2: {}", day2()?);
+    day3()?;
 
     Ok(())
 }
@@ -132,6 +190,12 @@ mod tests {
     fn test_day2() -> Result<()> {
         assert_eq!(calculate_rps_score(vec!["A Y", "B X", "C Z"])?, 12);
         assert_eq!(day2()?, 14859);
+        Ok(())
+    }
+    #[test]
+    fn test_day3() -> Result<()> {
+        assert_eq!(calculate_backpack_score(vec!["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"])?, 157);
+        assert_eq!(identify_group_badge(vec!["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"])?, 70);
         Ok(())
     }
 }
